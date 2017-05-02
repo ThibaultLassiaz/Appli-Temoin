89bcab2cbf24a759c2c647cb6cc6a1ff2c2ad839
Freyssinet Lucas <lfreyss_hp@hotmail.fr>
2017-05-02 13:59:15 +0200

    DatabaseManagerEtendu

---
+++ src/serveur/DatabaseManagerEtendu.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serveur;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import Serveur.database.DatabaseManager;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author Lucas
+ */
+public class DatabaseManagerEtendu extends DatabaseManager{
+    
+    /**
+     * Créer un utilisateur
+     *
+     * @param conn Connection
+     * @throws SQLException
+     */
+    public static void creationUtilisateur(Connection conn, String login, String password, String couleur) throws SQLException {
+        
+        try (Statement stmt = conn.createStatement()) {
+            //Récupère l'identifiant max
+            int idMax = 0;
+            ResultSet rset = stmt.executeQuery("SELECT max(idUt) from Utilisateur");
+            while (rset.next()) {
+                idMax = rset.getInt("max(idUt)") + 1;
+            }
+            //Creer l'utilisateur
+            stmt.executeQuery("insert into Utilisateur(idUt, login, motDePasse, couleur) values ( " + idMax + ", '"
+                    + login + "', '" + password + "', '" + couleur + "')");
+
+            conn.commit();
+            System.out.println("Utilisateur créé.");
+        } catch (SQLException e) {
+            conn.rollback();
+            System.out.println("Erreur de création de l'utilisateur : " + e.getMessage());
+        }
+
+    }
+}
